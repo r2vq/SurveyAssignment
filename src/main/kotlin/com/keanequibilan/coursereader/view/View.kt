c85ffa2ee98c35bd29fa0a88d41c3433c5c45f71
Keane Quibilan <keane.quibilan@loblaw.ca>
2021-03-27 19:50:17 -0400

    Create View Layer

--- src/main/kotlin/com/keanequibilan/coursereader/view/View.kt
+++ src/main/kotlin/com/keanequibilan/coursereader/view/View.kt
@@ -1,4 +1,13 @@
 package com.keanequibilan.coursereader.view
 
 interface View {
+    /**
+     * Print a message and return the user's input.
+     */
+    fun askForInput(message: String): String
+
+    /**
+     * Print a message.
+     */
+    fun printMessage(message: String)
 }
--- src/main/kotlin/com/keanequibilan/coursereader/view/impl/ViewImpl.kt
+++ src/main/kotlin/com/keanequibilan/coursereader/view/impl/ViewImpl.kt
@@ -3,4 +3,12 @@ package com.keanequibilan.coursereader.view.impl
 import com.keanequibilan.coursereader.view.View
 
 class ViewImpl : View {
+    override fun askForInput(message: String): String {
+        print("$message: ")
+        return readLine() ?: ""
+    }
+
+    override fun printMessage(message: String) {
+        println(message)
+    }
 }
